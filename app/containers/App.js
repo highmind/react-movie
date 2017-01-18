b8cb9d968041b29e8f2e04c4d1d4c0ec7fbea7a0
gyy <804798382@qq.com>
2017-01-18 14:55:08 +0800

    优化代码

--- app/containers/App.js
+++ app/containers/App.js
@@ -8,51 +8,36 @@ import {connect} from 'react-redux';
 class App extends Component{
     constructor(props){
         super(props);
-        // 相当于ES5 getInitalState,
         console.log('-------App--------')
         console.log('主容器App执行getInitialState')
         this.state = {
             navActive:false,
-            nav : [],        //导航数据
+            nav:[],        //导航数据
             data:[]
         }
-
         this.toggleNav = this.toggleNav.bind(this);
-        this.setNavActive = this.setNavActive.bind(this);
     }
 
-    // NavBar按钮点击，切换Nav的显示和隐藏
+    // NavBar按钮点击，切换Nav的显示和隐藏，且用于与Nav组件通信
     toggleNav(){
-      console.log('toggleNav');
       this.setState({
         navActive : !this.state.navActive
       })
     }
 
-    setNavActive(){
-      this.setState({
-        navActive : !this.state.navActive
-      });
-    }
-
     componentDidMount(){
         console.log('-------App--------')
         console.log('主容器App执行componentDidMount')
-        // 初始化数据
+        // 初始化导航数据
         let url = 'http://mockdata/get/nav';
         let self = this;
         let NavData = localStorage.getItem('NavData');
         // 如果导航数据在本地存在，则不请求远程数据
         if(NavData != null){
-            self.setState({
-                nav:JSON.parse(NavData)
-            })
+            self.setState({nav:JSON.parse(NavData)})
         }else{
             Axios.get(url).then(function(res){
-              console.log(res.data)
-                self.setState({
-                    nav:res.data.data
-                })
+                self.setState({nav:res.data.data})
                 localStorage.setItem("NavData", JSON.stringify(res.data.data));
             })
         }
@@ -76,14 +61,14 @@ class App extends Component{
         </a>
       ];
 
-        return(
+      return(
           <div>
               <NavBar name="芝麻电影"
                 leftContent={[navBarLeftNode]}
                 rightContent={[navBarRightNode]} />
               <Nav data={this.state.nav}
                 active={this.state.navActive}
-                setNavActive={this.setNavActive} />
+                setNavActive={this.toggleNav} />
               {React.cloneElement(this.props.children, this.props)}
           </div>
         )
